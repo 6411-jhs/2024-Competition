b01b67f9d42f9040d15f6636df845b97d80d05fc
Umbra <carsonjesseangell@gmail.com>
2024-02-26 17:41:12 -0700

    Finalized servo code

--- src/main/java/frc/robot/subsystems/Lifter.java
+++ src/main/java/frc/robot/subsystems/Lifter.java
@@ -7,26 +7,31 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Servo;
 
 public class Lifter extends SubsystemBase {
-   // private WPI_VictorSPX leftMotor;
-   // private WPI_VictorSPX rightMotor;
+   private WPI_VictorSPX leftMotor;
+   private WPI_VictorSPX rightMotor;
    private Servo servo;
    public Lifter(){
-      // leftMotor = new WPI_VictorSPX(Constants.CANAssignments.leftLCim);
-      // rightMotor = new WPI_VictorSPX(Constants.CANAssignments.rightLCim);
+      leftMotor = new WPI_VictorSPX(Constants.CANAssignments.leftLCim);
+      rightMotor = new WPI_VictorSPX(Constants.CANAssignments.rightLCim);
       servo = new Servo(0);
    }
 
-   // public void on(){
-   //    leftMotor.set(Constants.MAXSystemSpeeds.cims);
-   //    rightMotor.set(Constants.MAXSystemSpeeds.cims);
-   // }
+   public void on(){
+      leftMotor.set(Constants.MAXSystemSpeeds.cims);
+      rightMotor.set(Constants.MAXSystemSpeeds.cims);
+   }
 
-   // public void off(){
-   //    leftMotor.set(0);
-   //    rightMotor.set(0);
-   // }
+   public void off(){
+      leftMotor.set(0);
+      rightMotor.set(0);
+   }
 
+   /**
+    * Sets the servo to a specific degree measurment
+    * @param degree Any degree ranging from 0-180 (0 being start pos, 180 being 180 degrees)
+    */
    public void setServo(double degree){
-      servo.setAngle(degree);
+      double calcDegree = (150 / 180) * degree;
+      servo.setAngle(30 + calcDegree);
    }
 }
